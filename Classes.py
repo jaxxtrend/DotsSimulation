f8d81b044a38f0d5c4a6981f56a0a9240df125c4
Constantine Vlasov <jaxxtrend@gmail.com>
2021-07-31 15:38:04 +0500

    Sketching structures

---
+++ Classes.py
@@ -0,0 +1,42 @@
+"""structures"""
+
+class Location:
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
+
+class Product:
+    """product demanded from citizens"""
+    def __init__(self, name:str, type:str, price:int ):
+        self.name = name
+        self.type = type
+        self.price = price
+
+class City:
+    """Produce money income"""
+    def __init__(self, citizensCount:int, location:Location):
+        self.citizensCount = citizensCount
+        self.location = location
+
+class Factory:
+    """Made products for citizens and trading"""
+    def __init__(self, employers:int, product:str, resouces:dict):
+        self.employers = employers
+        self.product = product
+        self.resources = resouces
+
+class Citizen:
+    """Lives in cities"""
+    def __init__(self, wealth:int, work:Factory):
+        self.wealth = wealth
+        self.work = work
+
+class Transport:
+    """Delivery of products between the cities"""
+    def __init__(self, location:Location, cargo:dict, speed:int, destination:City, sentfrom:City):
+        self.location = location
+        self.cargo = cargo
+        self.speed = speed
+        self.destination = destination
+        self.sentfrom = sentfrom
+
