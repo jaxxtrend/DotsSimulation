96a6262b7dadb89facca9cba534fbc14853b272a
Constantine Vlasov <jaxxtrend@gmail.com>
2021-08-03 00:39:02 +0500

    salaries payd from city budget

--- Simulation.py
+++ Simulation.py
@@ -51,7 +51,7 @@ class LifeProcessor(esper.Processor):
                 citizen.age += 1
             else:
                 pass
-                #self.world.delete_entity(Citizen)
+                # self.world.delete_entity(Citizen)
 
 
 class WorkProcessor(esper.Processor):
@@ -59,18 +59,32 @@ class WorkProcessor(esper.Processor):
         super().__init__()
 
     def process(self):
+        averageSalary = 0
+        city = self.world.get_component(City)[0][1]
         for ent, (citizen) in self.world.get_component(Citizen):
             if citizen.age < 18:
-                pass
+                # 10% of the budget goes to providing for children
+                citizen.salary = int((city.budget * 0.1) / city.citizens)
             elif citizen.age >= 60 and citizen.age < 100:
-                citizen.salary = 10
+                # 20% of the budget goes to providing for the elderly
+                citizen.salary = int((city.budget * 0.2) / city.citizens)
             elif citizen.age >= 100:
                 citizen.salary = 0
             else:
-                citizen.salary += (random.randint(0, 6) * citizen.age)
-
+                # 50% of the budget goes to providing for the employs
+                citizen.salary = int((city.budget * 0.1) / city.citizens)
             citizen.money += citizen.salary
+            averageSalary += citizen.salary
+            # subtract salaty from budget
+            city.budget -= citizen.salary
+        #FAKE product price includes salaries by 50% of full price if sell this budget get profit.
+        city.budget += (averageSalary * 2)
+        # average salaries
+        averageSalary = int(averageSalary/city.citizens)
+        
+        print("salary = " + str(averageSalary))
             #print(ent, citizen.age, citizen.money, citizen.salary)
+
 
 class TaxProcessor(esper.Processor):
     def __init__(self) -> None:
@@ -82,12 +96,13 @@ class TaxProcessor(esper.Processor):
         for ent, (citizen) in self.world.get_component(Citizen):
             citizen.money = int(citizen.money - citizen.salary * (tax / 100))
             city.budget += (citizen.salary * (tax / 100))
-        #print(int(city.budget))
+        print(int(city.budget))
+
 
 class MigrationProcessor(esper.Processor):
     def __init__(self) -> None:
         super().__init__()
-    
+
     def process(self):
         pass
 
@@ -105,17 +120,16 @@ def main():
     # generate city
     cityName = "Moscow"
     world.create_entity(cityName)
-    world.add_component(cityName,City())
+    world.add_component(cityName, City(citizens=10000, budget=100000))
 
     # generate citizens
     i = 0
-    while i < 1:
+    while i < 10000:
         name = "citizen" + str(i)
         world.create_entity(name)
         world.add_component(name,
                             Citizen(age=random.randint(0, 30),
-                                    money=random.randint(0, 1000),
-                                    salary=random.randint(10, 30)))
+                                    money=random.randint(0, 1000)))
         i += 1
 
     try:
@@ -125,7 +139,7 @@ def main():
             world.process()
             time.sleep(0.1)
             i += 1
-            print(world.process)
+            # print(world.process)
 
     except KeyboardInterrupt:
         return
