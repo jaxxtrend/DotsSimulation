746250b777ae46f796ada658965ebcbf2ac6e708
Constantine Vlasov <jaxxtrend@gmail.com>
2021-08-09 01:02:23 +0500

    changed order

--- components.py
+++ components.py
@@ -2,11 +2,6 @@ from dataclasses import dataclass as component
 
 
 @component
-class Age:
-    v:int = 0
-
-
-@component
 class Citizen:
     pass
 
@@ -32,26 +27,31 @@ class Household:
 
 
 @component
+class Age:
+    v: int = 0
+
+
+@component
 class Name:
-    s:str = ""
+    s: str = None
 
 
 @component
 class Income:
-    v:float = 10.0
+    v: float = 0.0
 
 
 @component
 class Money:
-    v:float = 0.0
+    v: float = 0.0
+
 
 @component
 class TaxRate:
     """Value in precents"""
-    v:float = 20.0
+    v: float = 20.0
 
 
 @component
 class Employees:
-    v:int = 0
-
+    v: int = 0
