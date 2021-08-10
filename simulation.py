7b82f3e71b7c7ee8275d83253c63ed9177925b8b
Constantine Vlasov <jaxxtrend@gmail.com>
2021-08-11 02:29:13 +0500

    refactoring

--- simulation.py
+++ simulation.py
@@ -7,24 +7,18 @@ from entities import *
 
 
 def main():
+
+
     world = World()
+
     city = create_city(world)
-    factory = create_factory(world)
-    # generate citizens
-    i = 0
-    while i < 5:
-        citizen = create_citizen(world, age=randint(18, 30))
-        add_work(world, citizen, factory, 30, 13)
-        i += 1
-
+    # factory = create_factory(world)
+    
     # added processors
-    p_age = P_Age()
-    p_FactoryPaycheck = P_FactoryPaycheck()
-    p_getAGob = P_GetGob()
-    world.add_processor(p_age)
-    world.add_processor(p_FactoryPaycheck)
-    world.add_processor(p_getAGob)
+    world.add_processor(P_CitizenGeneration())
+    world.add_processor(P_Age())
 
+# one step is one day
     try:
         i = 0
         while True:
@@ -32,7 +26,6 @@ def main():
             world.process()
             time.sleep(0.1)
             i += 1
-            # print(len(world._entities))
 
     except KeyboardInterrupt:
         return
