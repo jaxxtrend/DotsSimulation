dd56d4ea9e58ea317046ebe9efdb32a38265f702
Constantine Vlasov <jaxxtrend@gmail.com>
2021-08-11 02:28:20 +0500

    added product entity generator, added return, formatted

--- entities.py
+++ entities.py
@@ -8,7 +8,10 @@ def create_city(world, name="Moscow", money=1000.0, taxRate=20.0):
     world.add_component(city, Name(s=name))
     world.add_component(city, Money(v=money))
     world.add_component(city, TaxRate(v=taxRate))
+    world.add_component(city, Population())
+    world.add_component(city, Childrens())
     print("City " + name + " created")
+    return city
 
 
 def create_factory(world, name="Basic"):
@@ -18,22 +21,31 @@ def create_factory(world, name="Basic"):
     world.add_component(factory, Name(s=name))
     world.add_component(factory, Money(v=1000))
     world.add_component(factory, TaxRate(v=40))
-    product = Product
-    product.name = "Energy"
-    product.cost = 0
-    product.price = 10
-    world.add_component(factory, product)
-    print("Factory " + name + " created")
+    print("Factory created")
+    return factory
 
 
-def create_citizen(world, age=0, money=0, income=0, taxRate=0):
+def create_product(world, name=""):
+    product = world.create_entity()
+    world.add_component(product, Product())
+    world.add_component(product, Name(s=name))
+    world.add_component(product, Cost())
+    world.add_component(product, Price())
+    print("Product " + name + " created")
+    return product
+
+
+def create_citizen(world, age=0, money=0,):
     citizen = world.create_entity()
     world.add_component(citizen, Citizen())
     world.add_component(citizen, Age(v=age))
     world.add_component(citizen, Money(v=money))
-   
+    print("Citizen created")
+    return citizen
+
 
 def add_work(world, ent, workId, salary=0, taxRate=0):
     world.add_component(ent, Salary(v=salary))
     world.add_component(ent, TaxRate(v=taxRate))
     world.add_component(ent, WorkId(v=workId))
+    print("Citizen work added")
