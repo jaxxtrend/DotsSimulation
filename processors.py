3430af46e10d1659ebc922197383bf83d9f64d41
Constantine Vlasov <jaxxtrend@gmail.com>
2021-08-12 02:34:22 +0500

    fixed cityzens generation and dead, update imports

--- processors.py
+++ processors.py
@@ -1,7 +1,8 @@
 from esper import Processor, World
-import components as cp
-from random import randint
+from entityComponents import *
+from entityTags import *
 from entities import create_citizen
+from random import randint
 
 
 class P_CitizenGeneration(Processor):
@@ -9,29 +10,31 @@ class P_CitizenGeneration(Processor):
         super().__init__()
 
     def process(self):
-        for ent, (city, childrens, population) in self.world.get_components(cp.City, cp.Childrens, cp.Population):
+        for ent, (city, childrens, population) in self.world.get_components(City, Childrens, Population):
             # generate citizens
             if len(childrens.v) < population.v:
-                citizens =[]
+                citizens = []
+                citizens += list(childrens.v)
                 while len(citizens) < population.v:
                     citizen = create_citizen(self.world, age=randint(18, 30))
                     citizens.append(citizen)
-                childrens.v += tuple(citizens)
+                childrens.v = tuple(citizens)
+                citizens = []
 
 class P_Age(Processor):
     def __init__(self):
         super().__init__()
 
     def process(self):
-        for ent, (age) in self.world.get_component(cp.Age):
+        for ent, (age) in self.world.get_component(Age):
             if age.v < 100:  # simple dead if age equal 100
                 age.v += 1
                 print(age)
             else:
-                for ent1,(childrens) in self.world.get_components(cp.City,cp.Childrens):
+                for ent1,(city, childrens) in self.world.get_components(City,Childrens):
                     y = list(childrens.v)
                     y.remove(ent)
-                    childrens.v = y
+                    childrens.v = tuple(y)
                 self.world.delete_entity(ent)
 
                 print("citizen is dead")
@@ -43,10 +46,10 @@ class P_Households(Processor):
         super().__init__()
 
     def process(self):
-        for ent, (type, Age) in self.world.get_component(cp.Age):
+        for ent, (type, age) in self.world.get_component(Age):
             if type.type == "citizen" and Age > 18:
-                Household = self.world.create_entity(cp.Household)
-        for ent, (Household) in self.world.get_component(cp.Household):
+                Household = self.world.create_entity(Household)
+        for ent, (Household) in self.world.get_component(Household):
             Household.citizens.append()
 
 
@@ -56,15 +59,15 @@ class P_Work(Processor):
 
     def process(self):
         incomeSum = 0
-        for ent, (income, money, taxRate) in self.world.get_components(cp.Income, cp.Money, cp.TaxRate):
-            if self.world.has_component(ent, component_type=cp.Citizen):
+        for ent, (income, money, taxRate) in self.world.get_components(Income, Money, TaxRate):
+            if self.world.has_component(ent, component_type=Citizen):
                 money.v += (income.v - income.v * (taxRate.v / 100))
                 incomeSum += (income.v - income.v * (taxRate.v / 100))
 
-        for ent, (income, money, taxRate) in self.world.get_components(cp.Income, cp.Money, cp.TaxRate):
-            if self.world.has_component(entity=ent, component_type=cp.City):
+        for ent, (income, money, taxRate) in self.world.get_components(Income, Money, TaxRate):
+            if self.world.has_component(entity=ent, component_type=City):
                 money.v += incomeSum * (taxRate.v / 100)
-            elif self.world.has_component(entity=ent, component_type=cp.Factory):
+            elif self.world.has_component(entity=ent, component_type=Factory):
                 money.v -= incomeSum
 
 
@@ -73,9 +76,9 @@ class P_Tax(Processor):
         super().__init__()
 
     def process(self):
-        tax = self.world.get_component(cp.City)[0][1].taxRate
-        city = self.world.get_component(cp.City)[0][1]
-        for ent, (citizen) in self.world.get_component(cp.Citizen):
+        tax = self.world.get_component(City)[0][1].taxRate
+        city = self.world.get_component(City)[0][1]
+        for ent, (citizen) in self.world.get_component(Citizen):
             citizen.money = int(citizen.money - citizen.income * (tax / 100))
             city.budget += (citizen.income * (tax / 100))
 
