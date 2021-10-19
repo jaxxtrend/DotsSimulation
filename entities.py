8dc8db8468c726dbab1917b5fb33bfe92512db7c
Constantine Vlasov <jaxxtrend@gmail.com>
2021-10-19 13:07:23 +0500

    Rerodered function

--- entities.py
+++ entities.py
@@ -3,6 +3,15 @@ from entityComponents import *
 from entityTags import *
 
 
+def create_citizen(world, age=0, money=0, health=100):
+    citizen = world.create_entity()
+    world.add_component(citizen, Citizen())
+    world.add_component(citizen, Age(v=age))
+    world.add_component(citizen, Health(v=health))
+    world.add_component(citizen, Money(v=money))
+    #print("Citizen created")
+    return citizen
+
 def create_city(world, name="Moscow", money=1000.0, taxRate=20.0):
     city = world.create_entity()
     world.add_component(city, City())
@@ -11,7 +20,7 @@ def create_city(world, name="Moscow", money=1000.0, taxRate=20.0):
     world.add_component(city, TaxRate(v=taxRate))
     world.add_component(city, Population())
     world.add_component(city, Childrens())
-    print("City " + name + " created")
+    #print("City " + name + " created")
     return city
 
 
@@ -22,7 +31,7 @@ def create_factory(world, name="Basic"):
     world.add_component(factory, Name(s=name))
     world.add_component(factory, Money(v=1000))
     world.add_component(factory, TaxRate(v=40))
-    print("Factory created")
+    #print("Factory created")
     return factory
 
 
@@ -32,21 +41,15 @@ def create_product(world, name=""):
     world.add_component(product, Name(s=name))
     world.add_component(product, Cost())
     world.add_component(product, Price())
-    print("Product " + name + " created")
+    #print("Product " + name + " created")
     return product
 
 
-def create_citizen(world, age=0, money=0,):
-    citizen = world.create_entity()
-    world.add_component(citizen, Citizen())
-    world.add_component(citizen, Age(v=age))
-    world.add_component(citizen, Money(v=money))
-    print("Citizen created")
-    return citizen
+
 
 
 def add_work(world, ent, workId, salary=0, taxRate=0):
     world.add_component(ent, Salary(v=salary))
     world.add_component(ent, TaxRate(v=taxRate))
     world.add_component(ent, WorkId(v=workId))
-    print("Citizen work added")
+    #print("Citizen work added")
