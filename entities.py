21e494f2b0908b540bfa3c8975bd6fc864fcdcd2
Constantine Vlasov <jaxxtrend@gmail.com>
2021-10-25 02:30:37 +0500

    Sqllite and picle modules tests

--- entities.py
+++ entities.py
@@ -1,9 +1,10 @@
 #from esper import World, Processor
 from entityComponents import *
 from entityTags import *
+#import logging
 
 
-def create_citizen(world, name="",family="", age=0, money=0, health=100):
+def create_citizen(world, name="", family="", age=0, money=0, health=100):
     citizen = world.create_entity()
     world.add_component(citizen, Citizen())
     world.add_component(citizen, Age(age))
@@ -11,17 +12,18 @@ def create_citizen(world, name="",family="", age=0, money=0, health=100):
     world.add_component(citizen, Money(money))
     world.add_component(citizen, Name(name))
     world.add_component(citizen, Family(family))
-    world.add_component(citizen, DeathTime())    
+    world.add_component(citizen, DeathTime())
     world.add_component(citizen, DeathChance())
-    #print("Citizen created")
+    #logging.info("Citizen created", citizen)
     return citizen
+
 
 def create_city(world, name="Moscow", money=1000.0, taxRate=20.0):
     city = world.create_entity()
     world.add_component(city, City())
     world.add_component(city, Name(name))
-    world.add_component(city, Money(v=money))
-    world.add_component(city, TaxRate(v=taxRate))
+    world.add_component(city, Money(money))
+    world.add_component(city, TaxRate(taxRate))
     world.add_component(city, Population())
     world.add_component(city, Childrens())
     #print("City " + name + " created")
@@ -32,9 +34,9 @@ def create_factory(world, name="Basic"):
     """create factory"""
     factory = world.create_entity()
     world.add_component(factory, Factory())
-    world.add_component(factory, Name(s=name))
-    world.add_component(factory, Money(v=1000))
-    world.add_component(factory, TaxRate(v=40))
+    world.add_component(factory, Name(name))
+    world.add_component(factory, Money(1000))
+    world.add_component(factory, TaxRate(40))
     #print("Factory created")
     return factory
 
@@ -42,18 +44,15 @@ def create_factory(world, name="Basic"):
 def create_product(world, name=""):
     product = world.create_entity()
     world.add_component(product, Product())
-    world.add_component(product, Name(s=name))
+    world.add_component(product, Name(name))
     world.add_component(product, Cost())
     world.add_component(product, Price())
     #print("Product " + name + " created")
     return product
 
 
-
-
-
 def add_work(world, ent, workId, salary=0, taxRate=0):
-    world.add_component(ent, Salary(v=salary))
-    world.add_component(ent, TaxRate(v=taxRate))
-    world.add_component(ent, WorkId(v=workId))
+    world.add_component(ent, Salary(salary))
+    world.add_component(ent, TaxRate(taxRate))
+    world.add_component(ent, WorkId(workId))
     #print("Citizen work added")
--- simulation.py
+++ simulation.py
@@ -6,12 +6,19 @@ from entityTags import *
 from processors import *
 from entities import *
 from constants import *
+from dataBase import create_connection, execute_query
+import json
+import pickle
+
 
 
 def main():    
 
-    world = World()
+    db = create_connection(":memory:")
+    backupDb = create_connection("history.db")
 
+    world = World()
+    
     city = create_city(world)
     # factory = create_factory(world)
     
@@ -19,7 +26,8 @@ def main():
     world.add_processor(CityzenSpawnProcessor())
     world.add_processor(AgeProcessor())
     world.add_processor(DeathProcessor())
-
+    
+    
     try:
         i = 0
         t = 0
@@ -27,6 +35,24 @@ def main():
             tick = MONTH
             # Call world.process() to run all Processors.
             world.process()
+                
+            pickle.dump(world, "dump.pcl", pickle.HIGHEST_PROTOCOL)
+
+            # worldDict = world.__dict__
+            # worldList = list(worldDict)
+            # worldJson = json.dumps(worldList)
+            # execute_query(backupDb, worldJson)
+
+            # for entity in world._entities:
+            #     entities = []
+            #     for comp in world.components_for_entity(entity):
+            #         componentsDict = {}
+            #         componentsDict.update(comp.__dict__)
+            #     entities.append(componentsDict)
+            # dumpStr = json.dumps(entities)
+            # #db.execute(str(dumpStr))
+
+
             i += 1
             if i % tick == 0:
                 et = time.process_time() - t
@@ -35,6 +61,9 @@ def main():
                     time.sleep(1-et)
                 print(et)
                 print("{}".format(str(timedelta(seconds=(60*i)))))
+                
+                #backup to the disk
+                db.backup(backupDb)
 
     except KeyboardInterrupt:
         return
